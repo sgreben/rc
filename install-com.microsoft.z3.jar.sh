3eb8eea636a5bb6260b9850700b684c30af3553f
sergey-grebenshchikov <sergey.grebenshchikov@codecentric.de>
2017-02-05 15:44:03 +0100

    Added shebang lines to shell scripts

--- install-com.microsoft.z3.jar.sh
+++ install-com.microsoft.z3.jar.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 mvn install:install-file -DgroupId=com.microsoft.z3 -DartifactId=z3 -Dversion=SNAPSHOT -Dfile=com.microsoft.z3.jar -Dpackaging=jar -Dtype=jar
--- install-libz3.dylib.sh
+++ install-libz3.dylib.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 mvn install:install-file -DgroupId=com.microsoft.z3 -DartifactId=libz3 -Dversion=SNAPSHOT -Dfile=libz3.dylib -Dpackaging=dylib -Dtype=dylib
